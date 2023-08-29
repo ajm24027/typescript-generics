1bbea4f2796eee48596a4fcabbdeaa89ea7e9f48
Anthony Medina <anthony.john.medina@gmail.com>
2023-08-29 08:15:36 -0400

    Generic Classes lesson complete

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,77 @@
-class Circle {
-  private _radius : number
+const numbers = [1, 2, 3]
+const strings = ['Hello', 'my', 'friend']
+const booleans = [true, true, false]
+interface Hero {
+  name: string;
+  superPower: string;
+}
 
-  constructor(radius) {
-    this._radius = radius
+const h1: Hero = {
+  name: "The flash",
+  superPower: "Run super fast",
+};
+
+const h2: Hero = {
+  name: "The Hulk",
+  superPower: "Super strong",
+};
+
+const h3: Hero = {
+  name: "Batman",
+  superPower: "Is rich",
+};
+
+const h4: Hero = {
+  name: "Superman",
+  superPower: "Basically a God",
+};
+
+const heroes = [h1, h2, h3, h4];
+
+
+// function removeLastElement(array: (number | string | boolean | Hero)[]): (number | string | boolean | Hero)[] {
+//  return array.slice(0, array.length - 1)
+// }
+
+function removeLastElement<MyType>(array: (MyType)[]): (MyType)[] {
+  return array.slice(0, array.length - 1)
+ }
+
+const newNumbers = removeLastElement(numbers)
+const newBooleans = removeLastElement(booleans)
+const newHeroes = removeLastElement(heroes)
+const newGenerics = removeLastElement<number>([1, 2 , 3])
+
+class SmartArray<T> {
+  private array: T[] = [];
+  constructor(array: T[]) {
+    this.array = array;
+  }
+  get values() {
+    return this.array;
   }
 
-  set radius(radius) {
-    if (radius > 0) {
-      this._radius = radius
-    } else {
-      throw new Error("Cannot set a negative radius")
-    }
+  shuffle(): T[] {
+    return this.array.sort(() => Math.random() - 0.5);
   }
 
-  get radius(): number {
-    return this._radius
+  push(element: T): void {
+    this.array.push(element);
+  }
+
+  removeLast(): T[] {
+    this.array.slice(0, this.array.length - 1);
+    return this.array;
   }
 }
 
-const circle = new Circle(5)
+const heroSmartArray = new SmartArray<Hero>(heroes);
 
-console.log(circle.radius)
+const shuffled = heroSmartArray.shuffle();
 
-circle.radius = 6
+console.log(shuffled);
 
-console.log(circle.radius)
+heroSmartArray.push({
+  name: "WonderWoman",
+  superPower: "fly",
+});
